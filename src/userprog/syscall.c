2869d02fa08609da790fc15ed9deeb5632a93802
ivanamies <Ivan>
2020-06-27 23:05:25 -0700

    pass tests/userprog/read-bad-fd

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -8,6 +8,7 @@
 #include "userprog/pagedir.h"
 #include "userprog/process.h"
 #include "devices/shutdown.h"
+#include "devices/input.h"
 #include "threads/synch.h"
 #include "threads/palloc.h"
 #include "filesys/inode.h"
@@ -23,13 +24,12 @@ static int check_user_ptr (void * p);
 #define MAX_PAGES_IN_FILE 8
 #define MAX_FILE_NAME_LEN 64
 #define MAX_FILES 128
+#define MAX_ARGS_ON_USER_STACK 4
 
 typedef struct fd_file {
   int fd;
   char file_name[MAX_FILE_NAME_LEN];
   struct file * file;
-  int sz;
-  int pos;
   int is_open; // 0 if this fd is closed, 1 if this fd is open
   int pid; // pid of owning process
 } fd_file_t;
@@ -67,7 +67,7 @@ static void destroy_fd_table(void) {
   lock_release(&fd_table_lock);
 }
 
-static int create_fd(const char * file_name, size_t sz, struct file * file) {
+static int create_fd(const char * file_name, struct file * file) {
   int i;
   int fd;
   int fd_idx;
@@ -79,16 +79,19 @@ static int create_fd(const char * file_name, size_t sz, struct file * file) {
   else if ( strlen(file_name)+1 >= MAX_FILE_NAME_LEN) {
     return -1;
   }
-    
+  
   lock_acquire(&fd_table_lock);
   
   fd_idx = -1;
   for ( i = 0; i < MAX_FILES; ++i ) {
     if ( fd_table[i].fd == -1 ) {
       fd_idx = i;
+      break;
     }
   }
-    
+  
+  /* printf("create_fd fd_idx: %d\n",fd_idx); */
+  
   if ( fd_idx == -1 ) {
     printf("ERROR: fd table is full\n");
     fd = -1;
@@ -99,15 +102,15 @@ static int create_fd(const char * file_name, size_t sz, struct file * file) {
   
   fd = fd_count; // must start at 2
   ++fd_count;
-  
+
   fd_table[fd_idx].fd = fd;
   strlcpy(fd_table[fd_idx].file_name,file_name,MAX_FILE_NAME_LEN);
-  fd_table[fd_idx].sz = sz;
-  fd_table[fd_idx].pos = 0;
   fd_table[fd_idx].file = file;
   fd_table[fd_idx].is_open = 1; // ONLY call from open_fd
   fd_table[fd_idx].pid = thread_pid();
-    
+
+  /* printf("fd_table[%d] %p fd %d\n",fd_idx,&fd_table[fd_idx],fd_table[fd_idx].fd); */
+  
  create_fd_done:
   lock_release(&fd_table_lock);
 
@@ -121,8 +124,8 @@ static int open_fd(const char * const file_name) {
     fd = -1;
     return fd;
   }
-    
-  fd = create_fd(file_name,file_length(file),file);
+  
+  fd = create_fd(file_name,file);
   
 
   return fd;
@@ -131,8 +134,6 @@ static int open_fd(const char * const file_name) {
 static void clear_fd(struct fd_file * fd_file) {
   fd_file->fd = -1;
   fd_file->file_name[0] = 0;
-  fd_file->sz = 0;
-  fd_file->pos = 0;
   fd_file->file = NULL;
   fd_file->is_open = 0;
   fd_file->pid = -1;
@@ -154,6 +155,54 @@ static void close_fd(int fd) {
   }
   
   lock_release(&fd_table_lock);
+}
+
+static int fd_to_fd_idx_no_lock(int fd) {
+  int i;
+  int fd_idx = -1;
+  for ( i = 0; i < MAX_FILES; ++i ) {
+    if ( fd_table[i].fd == fd ) {
+      fd_idx = i;
+      break;
+    }
+  }
+  return fd_idx;
+}
+
+static int is_valid_fd_entry_no_lock(int fd_idx) {
+  if ( fd_idx == -1 ) {
+    return 0;
+  }
+  else if ( fd_table[fd_idx].file == NULL ||
+            fd_table[fd_idx].is_open == 0 ) {
+    return 0;
+  }
+  return 1;
+}
+
+static int read_fd(int fd, void * p, unsigned sz) {
+  lock_acquire(&fd_table_lock);
+  int fd_idx = fd_to_fd_idx_no_lock(fd);
+  int ret = is_valid_fd_entry_no_lock(fd_idx);
+  if ( ret == 1 ) {
+    ASSERT(fd_table[fd_idx].pid == thread_pid());  
+    ret = file_read(fd_table[fd_idx].file,p,sz);
+  }
+  lock_release(&fd_table_lock);
+  return ret;
+}
+
+static int filesize_fd(int fd) {
+  lock_acquire(&fd_table_lock);
+  int fd_idx = fd_to_fd_idx_no_lock(fd);
+  int ret = is_valid_fd_entry_no_lock(fd_idx);
+  if ( ret == 1 ) {
+    ASSERT(fd_table[fd_idx].file != NULL);
+    ret = file_length(fd_table[fd_idx].file);
+  }
+  lock_release(&fd_table_lock);
+  return ret;
+  
 }
 
 void
@@ -213,6 +262,54 @@ static int check_user_ptr_with_terminate(void * p) {
   }
 }
 
+static int get_num_args(int syscall_no) {
+  int num_args;
+  if ( syscall_no == SYS_HALT ) {
+    num_args = 0;
+  }
+  else if (syscall_no == SYS_EXIT ) {
+    num_args = 1;
+  }
+  else if ( syscall_no == SYS_EXEC ) {
+    num_args = 1;
+  }
+  else if ( syscall_no == SYS_WAIT ) {
+    num_args = 1;
+  }
+  else if ( syscall_no == SYS_CREATE ) {
+    num_args = 2;
+  }
+  else if ( syscall_no == SYS_REMOVE ) {
+    num_args = 1;
+  }
+  else if ( syscall_no == SYS_OPEN ) {
+    num_args = 1;
+  }
+  else if ( syscall_no == SYS_FILESIZE ) {
+    num_args = 1;
+  }
+  else if ( syscall_no == SYS_READ ) {
+    num_args = 3;
+  }
+  else if ( syscall_no == SYS_WRITE ) {
+    num_args = 3;
+  }
+  else if ( syscall_no == SYS_SEEK ) {
+    num_args = 2;
+  }
+  else if ( syscall_no == SYS_TELL ) {
+    num_args = 1;
+  }
+  else if ( syscall_no == SYS_CLOSE ) {
+    num_args = 1;
+  }
+  else {
+    ASSERT(false);
+    num_args = 0;
+  }
+  return num_args;
+}
+
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
@@ -235,11 +332,26 @@ syscall_handler (struct intr_frame *f UNUSED)
   if ( check_user_ptr_with_terminate(esp) ) {
     return;
   }
-    
+  
   syscall_no = *((int *)esp);
   /* printf("syscall_no: %d\n",syscall_no); */
   esp += word_size;
 
+  int num_args = get_num_args(syscall_no);
+  void * user_args[MAX_ARGS_ON_USER_STACK];
+  memset(user_args,0,MAX_ARGS_ON_USER_STACK*sizeof(void *));
+
+  // only do this for syscall read for now
+  if ( syscall_no == SYS_READ || syscall_no == SYS_FILESIZE ) { 
+    for ( int i = 0; i < num_args; ++i ) {
+      if ( check_user_ptr_with_terminate(esp) ) {
+        return;
+      }
+      user_args[i] = *((void **)esp);
+      esp += word_size;
+    }
+  }
+  
   if ( syscall_no == SYS_HALT ) {
     shutdown_power_off();
   }
@@ -301,9 +413,30 @@ syscall_handler (struct intr_frame *f UNUSED)
     f->eax = fd;
   }
   else if ( syscall_no == SYS_FILESIZE ) {
+    int fd = (int)user_args[0];
+    f->eax = filesize_fd(fd);
   }
   else if ( syscall_no == SYS_READ ) {
-    // blah
+    int fd = (int)user_args[0];
+    void * p = user_args[1];
+    unsigned sz = (unsigned)user_args[2];
+    if ( check_user_ptr_with_terminate(p) ) {
+      return;
+    }
+    if ( fd == 0 ) {
+      // read from keyboard
+      unsigned num_read = 0;
+      char key;
+      while ( num_read < sz ) {
+        key = input_getc();
+        memset(p,key,1);
+        ++num_read;
+      }
+      f->eax = sz;
+    }
+    else {
+      f->eax = read_fd(fd,p,sz);      
+    }
   }
   else if ( syscall_no == SYS_WRITE ) {
     if ( check_user_ptr_with_terminate(esp) ) {
