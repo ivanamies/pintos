4e349416887499df56947847a454fcf15604d0eb
ivanamies <Ivan>
2020-09-10 21:52:50 -0700

    realize a list would be a bad idea

--- src/vm/swap.c
+++ src/vm/swap.c
@@ -9,7 +9,7 @@
 #include <stdio.h>
 
 typedef struct swap_page {
-  struct hash_elem hash_elem;
+  struct list_elem list_elem;
   // block sector sizes are 512
   // each page has 8 sectors
   block_sector_t sector;
@@ -19,28 +19,12 @@ typedef struct swap_table {
   struct lock lock;
   
   struct block * block;
-  struct hash available_block_pages;
-  struct hash unavailable_block_pages;
+  struct list available_block_pages;
+  struct list unavailable_block_pages;
   
 } swap_table_t;
 
 static swap_table_t swap_table;
-
-static unsigned swap_page_hash(const struct hash_elem * p_,
-                                     void * aux UNUSED) {
-  const swap_page_t * p = hash_entry(p_, swap_page_t, hash_elem);
-  unsigned res = hash_int(p->sector);
-  return res;
-}
-
-static bool swap_page_less(const struct hash_elem * a_,
-                           const struct hash_elem * b_,
-                           void * aux UNUSED ) {
-  const swap_page_t * a = hash_entry(a_, swap_page_t, hash_elem);
-  const swap_page_t * b = hash_entry(b_, swap_page_t, hash_elem);
-  bool res = a->sector < b->sector;
-  return res;
-}
 
 void swap_init(void) {
   swap_page_t * page;
@@ -50,8 +34,8 @@ void swap_init(void) {
   size_t num_pages;
   
   lock_init(&swap_table.lock);
-  hash_init(&swap_table.available_block_pages,swap_page_hash,swap_page_less,NULL);
-  hash_init(&swap_table.unavailable_block_pages,swap_page_hash,swap_page_less,NULL);
+  list_init(&swap_table.available_block_pages);
+  list_init(&swap_table.unavailable_block_pages);
 
   swap_table.block = block_get_role(BLOCK_SWAP);
   num_blocks = block_size(swap_table.block);
@@ -63,7 +47,7 @@ void swap_init(void) {
   for ( i = 0; i < num_pages; ++i ) {
     page = (swap_page_t *)malloc(sizeof(swap_page_t));
     page->sector = (PGSIZE / BLOCK_SECTOR_SIZE)*i; // each page is 8 blocks in length
-    hash_out = hash_insert(&swap_table.available_block_pages,&page->hash_elem);
+    list_push_back(&swap_table.available_block_pages,&page->list_elem);
   }
 }
 
@@ -77,23 +61,22 @@ void swap_deinit() {
 block_sector_t swap_write_page(void * p_, size_t sz) {
   uint8_t * p;
   swap_page_t * out;
-  struct hash_elem * hash_out;
+  struct list_elem * list_out;
   block_sector_t sector;
   size_t sectors_read;
   const size_t max_sectors_read = PGSIZE / BLOCK_SECTOR_SIZE; // == 8
-  struct hash_iterator i;
 
   ASSERT(sz == PGSIZE);
   
   ////
   lock_acquire(&swap_table.lock);
   
-  ASSERT (!hash_empty(&swap_table.available_block_pages));
+  ASSERT (!list_empty(&swap_table.available_block_pages));
   
   // fill hash iterator
-  hash_first(&i,&swap_table.available_block_pages);
-  hash_next(&i);
-  out = hash_entry(hash_cur(&i), swap_page_t, hash_elem);
+  list_out = list_pop_front(&swap_table.available_block_pages);
+  ASSERT(list_out != NULL);
+  out = list_entry(list_out, swap_page_t, list_elem);
   sector = out->sector;
 
   lock_release(&swap_table.lock);
@@ -111,10 +94,7 @@ block_sector_t swap_write_page(void * p_, size_t sz) {
   lock_acquire(&swap_table.lock);
   
   // remove out from available_block_pages and send to unavailable_block_pages
-  hash_out = hash_delete(&swap_table.available_block_pages,&out->hash_elem);
-  ASSERT(hash_out != NULL);
-  hash_out = hash_insert(&swap_table.unavailable_block_pages,&out->hash_elem);
-  ASSERT(hash_out == NULL);
+  list_out = list_push_back(&swap_table.unavailable_block_pages,&out->list_elem);
   
   lock_release(&swap_table.lock);
   ////
@@ -127,7 +107,7 @@ void swap_get_page(void * p_, size_t sz, block_sector_t sector) {
   ASSERT(sz == PGSIZE);
 
   uint8_t * p;
-  struct hash_elem * hash_out;
+  struct list_elem * list_out;
   swap_page_t key;
   /* swap_page_t * out; */
   size_t sectors_read;
@@ -142,6 +122,7 @@ void swap_get_page(void * p_, size_t sz, block_sector_t sector) {
   
   lock_acquire(&swap_table.lock);
   // remove from unavailable pages, add back to available pages
+  list_out = list_
   hash_out = hash_delete(&swap_table.unavailable_block_pages,&key.hash_elem);
   ASSERT(hash_out != NULL);
   hash_out = hash_insert(&swap_table.available_block_pages,&key.hash_elem);
