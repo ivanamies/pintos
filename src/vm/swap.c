34b572fb16fff77a5808937cafa757ae0bf3ac14
ivanamies <Ivan>
2020-09-20 17:36:59 -0700

    try to fix race conditons across kpages but fail

--- src/vm/swap.c
+++ src/vm/swap.c
@@ -4,6 +4,7 @@
 #include "threads/malloc.h"
 #include "threads/synch.h"
 #include "threads/vaddr.h"
+#include "threads/thread.h"
 
 #include <kernel/hash.h>
 #include <stdio.h>
@@ -122,9 +123,9 @@ block_sector_t swap_write_page(void * p_, size_t sz) {
   // this is synchronized for you
   for ( sectors_read = 0; sectors_read < max_sectors_read; ++sectors_read ) {
     p = p_ + sectors_read * BLOCK_SECTOR_SIZE;
-    /* printf("t %p tagiamies 9 sector %zu\n",thread_current(),sector); */
+    printf("t %p tagiamies 9 sector %zu\n",thread_current(),sector);
     block_write(swap_table.block,sector+sectors_read,p);
-    /* printf("t %p tagiamies 10\n",thread_current()); */
+    printf("t %p tagiamies 10\n",thread_current());
   }
 
   ////
@@ -157,7 +158,9 @@ void swap_get_page(void * p_, size_t sz, block_sector_t sector) {
   
   for ( sectors_read = 0; sectors_read < max_sectors_read; ++sectors_read ) {
     p = p_ + sectors_read * BLOCK_SECTOR_SIZE;
+    printf("t %p tagiamies 13 sector %zu\n",thread_current(),sector);
     block_read(swap_table.block, sector + sectors_read, p);
+    printf("t %p tagiamies 14 sector %zu\n",thread_current(),sector);
   }
   
   /* printf("t %p swap get page acquire lock\n",thread_current()); */
